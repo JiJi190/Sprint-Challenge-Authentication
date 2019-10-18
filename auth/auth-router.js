137f0d391599e6a16d35a1c94bfd49cf11ade89b
unknown <robertrey1234@gmail.com>
2019-10-18 10:18:38 -0500

    Added dependencies, created helper functions

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bycryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("./auth-config.js");
+const restricted = require("./authenticate-middleware.js");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
